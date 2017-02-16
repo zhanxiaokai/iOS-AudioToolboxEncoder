06ecdd0ad7710c7b7df3fc3e3266070e96b54836
xiaokai.zhan <zhanxiaokai@changba.com>
2017-02-16 17:49:07 +0800

    Initial Commit

---
+++ AudioToolboxEncoder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioToolboxEncoder
+//
+//  Created by apple on 2017/2/16.
+//  Copyright © 2017年 xiaokai.zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
